11a117ef495e59b059b2fa7011e9cf3e81959b9d
gabriel <c.a.gabriel2011@hotmail.com>
2024-05-13 20:28:42 -0300

    mudanca de posicao e sprite das juntas

--- Assets/Game/codigos/jogo.cs
+++ Assets/Game/codigos/jogo.cs
@@ -28,6 +28,17 @@ public class jogo : MonoBehaviour
         som.Play();
         
         back_render.sprite = backgrunds[num_tela];
+        back_render2.sprite = backgrunds[num_tela];
+
+        junta1.GetComponent<SpriteRenderer>().sprite = backgrunds_juntas[num_tela];
+        junta2.GetComponent<SpriteRenderer>().sprite = backgrunds_juntas[num_tela];
+
+
+        if (num_tela == 0) {local_juntas(15.02f, 0, 1.8f, 2);}
+        else if (num_tela == 1) { local_juntas(18.45f, 0, 2, 2); }
+        else if (num_tela == 2) { local_juntas(15.76f, 0, 1.8f, 2); }
+        else if (num_tela == 3) { local_juntas(16.76f, 0, 1.8f, 2); }
+        else if (num_tela == 4) { local_juntas(16.76f, 0, 1.8f, 2); }
         
           //  telas[num_tela].SetActive(true);
             for (int i = 0; i < telas.Length; i++)
@@ -40,6 +51,13 @@ public class jogo : MonoBehaviour
 
     }
 
+    public void local_juntas(float local1,float local2,float escala,float escala2) {
+        junta1.GetComponent<Transform>().localPosition = new Vector2(local1, local2);
+        junta1.GetComponent<Transform>().localScale = new Vector2(escala, escala2);
+
+        junta2.GetComponent<Transform>().localPosition = new Vector2(local1, local2);
+        junta2.GetComponent<Transform>().localScale = new Vector2(escala, escala2);
+    }
     // Update is called once per frame
     void Update()
     {
