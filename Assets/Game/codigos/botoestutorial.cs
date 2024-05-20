575d90c25a5031073f628982cf8dbf3322f6e602
gabriel <c.a.gabriel2011@hotmail.com>
2024-05-20 15:28:51 -0300

    ajustando posicao dos ementos da ui de tutorial de controles

---
+++ Assets/Game/codigos/botoestutorial.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class botoestutorial : MonoBehaviour
+{
+    public Text direita_text; 
+    public Text esquerda_text;
+    public Text pulo_text;
+    public Text katana_text;
+    public Text especial_text;
+    public Text coletar_text;
+    public Text combinar_text;
+  //  public Text sair_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+            direita_text.text = controle_config.chave_direita+"";
+            esquerda_text.text = controle_config.chave_esquerda+"";
+            pulo_text.text = controle_config.chave_pulo+"";
+            katana_text.text = controle_config.chave_katana+"";
+            especial_text.text = controle_config.chave_especial+"";
+            coletar_text.text = controle_config.chave_coletar+"";
+            combinar_text.text = controle_config.chave_combinar+"";
+            //sair_text.text = controle_config.chave_sair+"";
+        
+    }
+
+    
+}
