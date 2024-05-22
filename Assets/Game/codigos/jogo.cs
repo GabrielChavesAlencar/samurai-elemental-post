350c9ee60a8cf90179c477bcc8c1be5bb0b01478
gabriel <c.a.gabriel2011@hotmail.com>
2024-05-22 19:07:06 -0300

    ajustando posicao dos backgroudntelas 6 a 8

--- Assets/Game/codigos/jogo.cs
+++ Assets/Game/codigos/jogo.cs
@@ -20,7 +20,7 @@ public class jogo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      //  num_tela = 6;
+        num_tela = 7;
        
         som = GetComponent<AudioSource>();
         som.clip = musicas[num_tela];
@@ -39,9 +39,9 @@ public class jogo : MonoBehaviour
         else if (num_tela == 2) { local_juntas(15.76f, 0, 1.8f, 2); }
         else if (num_tela == 3) { local_juntas(16.76f, 0, 1.8f, 2); }
         else if (num_tela == 4) { local_juntas(16.76f, 0, 1.8f, 2); }
-        else if (num_tela == 5) { local_juntas(18.14f, 0, 1.8f, 2); }
+        else if (num_tela == 5) { local_juntas(22.97f, 0, 2.2f, 2); }
         else if (num_tela == 6) { local_juntas(25.35f, 0, 2.2f, 2); }
-        else if (num_tela == 7) { local_juntas(17.97f, 0, 2, 2); }
+        else if (num_tela == 7) { local_juntas(26.62f, 0, 2.45f, 2); }
         else if (num_tela == 8) { local_juntas(15.94f, 0, 1.8f, 2); }
         else if (num_tela == 9) { local_juntas(15.94f, 0, 1.8f, 2); }
         
