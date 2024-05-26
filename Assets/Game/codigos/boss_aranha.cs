3b054956bf659a1bf66e9b6398be83d305b493b8
gabriel <c.a.gabriel2011@hotmail.com>
2024-05-26 20:48:01 -0300

    melhorando mecanica do boss aranha

--- Assets/Game/codigos/boss_aranha.cs
+++ Assets/Game/codigos/boss_aranha.cs
@@ -25,13 +25,48 @@ public class boss_aranha : mob
     // Update is called once per frame
     void Update()
     {
+        if (estado == "morte")
+        {
+             
+            desativar_dano();
+            anim.Play("morte");
+            tempo_morte -= Time.deltaTime / 3;
+            if (tempo_morte > 0.95 && tempo_morte < 1)
+            {
+                tempo_morte = 0.94f;
+                float random = Random.Range(0, 10);
+                hud.almas++;
+                //   print(random);
+                if (drop_garantido) { random = 0; }
+                if (atual.Elite) { random = Random.Range(0, 6); }
+                
+                if (random < 3)
+                {
+                    if (elemento == "fogo") { uis.invocar_poder(transform.position, 0, "fogo"); }
+                    else if (elemento == "agua") { uis.invocar_poder(transform.position, 1, "agua"); }
+                    else if (elemento == "pedra") { uis.invocar_poder(transform.position, 2, "pedra"); }
+                    else if (elemento == "eletrico") { uis.invocar_poder(transform.position, 3, "eletrico"); }
+                    else if (elemento == "vento") { uis.invocar_poder(transform.position, 4, "vento"); }
+                }
+            }
+
+            if (tempo_morte < 0) { tempo_morte = 0; vivo = true; gameObject.SetActive(false); }
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, tempo_morte);
+            GetComponent<CapsuleCollider2D>().enabled = false;
+            rig.velocity = new Vector2(0, 0);
+        }
+        if (vidaatual <= 0) { estado = "morte";anim.Play("morte"); }
         distancia = Vector2.Distance(player.transform.position, transform.position);
 
         
-
-        if(vidaatual<200&&invocar == 0){indice_escudo=5;}
-        else if(vidaatual<450&&invocar == 0){indice_escudo=3;}
-        else if(vidaatual<800&&invocar == 0){indice_escudo=1;}
+        if(invocar==0){
+            if(indice_escudo==1||indice_escudo==3||indice_escudo==5){
+                indice_escudo++;
+            }
+        }
+        if(vidaatual<200&&invocar == 0&&indice_escudo==4){indice_escudo=5;}
+        else if(vidaatual<450&&invocar == 0&&indice_escudo==2){indice_escudo=3;}
+        else if(vidaatual<800&&invocar == 0&&indice_escudo==0){indice_escudo=1;}
 
         if(indice_escudo==1||indice_escudo==3||indice_escudo==5){
             campo_forca.SetActive(true);
@@ -42,18 +77,26 @@ public class boss_aranha : mob
 
             if(invocar == 0&&minon1!=null&&minon2!=null&&minon3!=null){
                 invocar = 3;
+                minon1.SetActive(true);
+                minon2.SetActive(true);
+                minon3.SetActive(true);
                 minon1.GetComponent<mob>().desativar_distancia = true;
                 minon2.GetComponent<mob>().desativar_distancia = true;
                 minon3.GetComponent<mob>().desativar_distancia = true;
+
+                minon1.GetComponent<contadorminion>().bosses = this;
+                minon2.GetComponent<contadorminion>().bosses = this;
+                minon3.GetComponent<contadorminion>().bosses = this;
+
                 if (transform.position.x > player.transform.position.x) {
-                    minon1.transform.position = transform.position + new Vector3(-10,0,0);
+                    minon1.transform.position = transform.position + new Vector3(-15,0,0);
                     minon2.transform.position = transform.position + new Vector3(-20,0,0);
-                    minon3.transform.position = transform.position + new Vector3(-30,0,0);
+                    minon3.transform.position = transform.position + new Vector3(-25,0,0);
                 }
                 else{
-                    minon1.transform.position = transform.position + new Vector3(10,0,0);
+                    minon1.transform.position = transform.position + new Vector3(15,0,0);
                     minon2.transform.position = transform.position + new Vector3(20,0,0);
-                    minon3.transform.position = transform.position + new Vector3(30,0,0);
+                    minon3.transform.position = transform.position + new Vector3(35,0,0);
                 }
 
 
--- Assets/Game/codigos/contadorminion.cs
+++ Assets/Game/codigos/contadorminion.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class contadorminion : MonoBehaviour
 {
+    public boss_aranha bosses;
     
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,7 @@ public class contadorminion : MonoBehaviour
   
     private void OnDisable()
     {
-        print("funcionou");
+        //print("funcionou");
+        bosses.invocar--;
     }
 }
