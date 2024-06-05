cd3ae5ee5a031168ec930b74521e7b38b35895e9
gabriel <c.a.gabriel2011@hotmail.com>
2024-06-05 16:45:07 -0300

    configurando paleta areia

--- Assets/Game/codigos/jogo.cs
+++ Assets/Game/codigos/jogo.cs
@@ -20,7 +20,7 @@ public class jogo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        num_tela = 9;
+        //num_tela = 9;
        
         som = GetComponent<AudioSource>();
         som.clip = musicas[num_tela];
