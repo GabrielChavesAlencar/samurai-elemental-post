ad6cee30524c537b81edbd2902633ab2005e16f4
gabriel <c.a.gabriel2011@hotmail.com>
2024-06-11 18:08:18 -0300

    melhorando animação golpe queda

--- Assets/Game/codigos/jogo.cs
+++ Assets/Game/codigos/jogo.cs
@@ -20,7 +20,7 @@ public class jogo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        num_tela = 10;
+        //num_tela = 10;
        
         som = GetComponent<AudioSource>();
         som.clip = musicas[num_tela];
